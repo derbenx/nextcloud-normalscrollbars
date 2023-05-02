fbea924ae3263d44a4795d75c6042f739c1cb0c6
derbenx <colin@derben.ca>
2023-05-01 22:36:04 -0400

    Update MakePackage.sh

--- code/MakePackage.sh
+++ code/MakePackage.sh
@@ -3,19 +3,19 @@
 #Made by @OliverParoczai
 #Requires git, xmlstarlet, tar, and optionally openssl
 
-REPOURL="https://github.com/OliverParoczai/nextcloud-unroundedcorners.git" #official repo, change if forked
+REPOURL="https://github.com/derbenx/nextcloud-normalscrollbars.git" #official repo, change if forked
 SIGN=FALSE #Sign the file for uploading to the official appstore
-SIGNCERT=~/.nextcloud/certificates/unroundedcorners.key #The location of the key
+SIGNCERT=~/.nextcloud/certificates/normalscrollbars.key #The location of the key
 
 mkdir tmp
 
 echo "Cloning $REPOURL"
-git clone -q $REPOURL tmp/unroundedcorners/
-VERSION=$(xmlstarlet sel -T -t -m '//info/version' -v . -n tmp/unroundedcorners/appinfo/info.xml)
+git clone -q $REPOURL tmp/normalscrollbars/
+VERSION=$(xmlstarlet sel -T -t -m '//info/version' -v . -n tmp/normalscrollbars/appinfo/info.xml)
 echo "Version is ${VERSION}"
 
 echo "Starting compression"
-tar --exclude='code' --exclude='differences.png' --exclude=".git" -czf unroundedcorners-v${VERSION}.tar.gz -C tmp unroundedcorners/
+tar --exclude='code' --exclude='differences.png' --exclude=".git" -czf normalscrollbars-v${VERSION}.tar.gz -C tmp normalscrollbars/
 
 rm -rf tmp
 
@@ -23,6 +23,6 @@ if [ $SIGN ]; then
 	echo ""
 	echo "The signature:"
 	echo ""
-	openssl dgst -sha512 -sign $SIGNCERT unroundedcorners-v${VERSION}.tar.gz | openssl base64
+	openssl dgst -sha512 -sign $SIGNCERT normalscrollbars-v${VERSION}.tar.gz | openssl base64
 	echo ""
 fi
